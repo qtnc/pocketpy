15e030b132b1854d98178a3b77a6d98380f1bed0
QuentinC <webmaster@quentinc.net>
2023-12-30 23:50:15 +0100

    Locale-independent strtod_c

--- src/strtod_c.cpp
+++ src/strtod_c.cpp
@@ -1,12 +1,7 @@
 #include<clocale>
 #include<cstdlib>
 
-double strtod_c (const char* s, char** e) {
-static _locale_t cLocale;
-static bool init = false;
-if (!init) {
-cLocale = _create_locale(LC_ALL, "c");
-init = true;
-}
-return _strtod_l(s, e, cLocale);
+double strtod_c (const char* s, char** endptr = nullptr) {
+static _locale_t c = _create_locale(LC_ALL, "C");
+return _strtod_l(s, endptr, c);
 }
